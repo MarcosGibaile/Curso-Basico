513046c0ba3e880cc5c59819235f275f08fa37c1
Marcos <digimaq.ituveravav@gmail.com>
2019-08-04 11:51:20 -0300

    voltei comentario

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-
+/*======*/
 namespace SalesWebMvc
 {
     public class Program
